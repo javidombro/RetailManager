60f3f03a37d125e67801f955be098079324d9c2c
javi.dombro <javi.dombro@outlook.com>
2020-10-17 19:25:13 -0300

    Add Swagger Documentation to API
    Adds Swashbuckle/Swagger to the API
    for documentation and testing

---
+++ RMDataManager/App_Start/AuthTokenOperation.cs
@@ -0,0 +1,52 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+namespace RMDataManager.App_Start
+{
+    public class AuthTokenOperation : IDocumentFilter
+    {
+        public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, IApiExplorer apiExplorer)
+        {
+            swaggerDoc.paths.Add("/token", new PathItem //Adds new Method
+            {
+                post = new Operation
+                {
+                    tags = new List<string> { "Auth" }, // In the auth cathegory
+                    consumes = new List<string>
+                    {
+                        "application/x-www-form-urlencoded"
+                    },
+                    parameters = new List<Parameter>    // with this parameters
+                    {
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "grant_type",
+                            required = true,
+                            @in = "formData",
+                            @default = "password"
+                        },
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "username",
+                            required = false,
+                            @in = "formData"
+                        },
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "password",
+                            required = false,
+                            @in = "formData"
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
