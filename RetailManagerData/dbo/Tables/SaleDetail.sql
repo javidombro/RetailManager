57feb4adab696509436b9ea956da2aba32bc39a2
Javier Dombronsky <jdombronsky@hexacta.com>
2020-10-22 22:30:40 -0300

    Adds Data Base for the model

---
+++ RetailManagerData/dbo/Tables/SaleDetail.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[SaleDetail]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [SaleId] INT NULL, 
+    [ProductId] INT NULL, 
+    [Quantity] INT NULL DEFAULT 1,
+    [PurchasePrice] MONEY NULL, 
+    [Tax] MONEY NULL DEFAULT 0,
+)
